b4bd8cee06d19f7ca2905f3df3bb3f63117f9331
Jonathan-Gomez <gomez.e.jonathan@gmail.com>
2018-08-23 17:22:21 -0400

    Display the cards when clicked

--- js/app.js
+++ js/app.js
@@ -1,14 +1,7 @@
-/*
- * Create a list that holds all of your cards
- */
-const cards = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-bomb", "fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle", "fa-paper-plane-o", "fa-cube"]
+// Create a list that holds all of your cards
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+const listOfCards = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-bomb", "fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle", "fa-paper-plane-o", "fa-cube"]
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -26,28 +19,37 @@ function shuffle(array) {
 }
 
 // Shuffle list of cards
-shuffle(cards);
+shuffle(listOfCards);
 
 // Creates HTML for each card in shuffled deck, then adds the HTML to the page.
 deck = document.querySelector('.deck');
 
-for (card in cards) {
+for (card in listOfCards) {
 	parentOfList = document.createElement('div');
 	newList = parentOfList.insertAdjacentElement('beforeend', document.createElement('li'));
-	newList.classList.add('card', 'open', 'show')
+	newList.classList.add('card')
 	childOfList = newList.insertAdjacentElement('beforeend', document.createElement('i'));
-	childOfList.classList.add('fa', cards[card]);
+	childOfList.classList.add('fa', listOfCards[card]);
 	parentOfList.appendChild(newList);
 	deck.appendChild(newList);
 	}
 
-
-
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+*/
+function clicked(ev){
+  if (ev.target.classList.contains('card')) {
+    ev.target.classList.toggle('open')
+    ev.target.classList.toggle('show')
+  }
+  ev.stopPropagation();
+}
+
+document.querySelector('.deck').addEventListener('click',clicked);
+
+
+ /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
