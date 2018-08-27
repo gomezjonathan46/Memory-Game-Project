b5c8a6914a4444d06d6051c6329337b2eedb2ce0
Jonathan-Gomez <gomez.e.jonathan@gmail.com>
2018-08-27 14:18:16 -0400

    Stop adding matched cards to 'open' list

--- js/app.js
+++ js/app.js
@@ -76,11 +76,14 @@ function checkForMatch(ev){
   }
 }
 
-document.querySelector('.deck').addEventListener('click', checkForMatch);
-
 function unclickable(item) {
-
+  item.preventDefault();
+  item.stopPropagation();
 }
+
+document.querySelector('.deck').addEventListener('click', checkForMatch);
+document.querySelector('.match').addEventListener('click', unclickable);
+
 
 //    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
 //    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
