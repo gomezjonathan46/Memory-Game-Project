16a625b77e843d038b09a31c99e10154fb71dbd6
Jonathan-Gomez <gomez.e.jonathan@gmail.com>
2018-09-05 11:44:57 -0400

    Create a stylized pop-up window with buttons

--- js/app.js
+++ js/app.js
@@ -90,7 +90,7 @@ function checkForMatch(ev){
         match++
         if (match === 16) {
           stopTimer();
-          document.querySelector('.modal').style.display = "block"
+          document.querySelector('.modal-background').style.display = "block"
         }
       })
       open = [];
